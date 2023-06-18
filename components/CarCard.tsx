c0c4ba7119ef60ea4a62df6c707a3d540c69506f
Adam Gordon <adamg42685@gmail.com>
2023-06-17 20:57:31 -0400

    tweak styling

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -17,9 +17,8 @@ const CarCard = ({ car }: CarCardProps) => {
   return (
     <div className="car-card group">
       <div className="car-card__content">
-        <h2 className="car-card__title">
-          {make}
-          {model}
+        <h2 className="text-[22px] leading-[26px] font-bold capitalize">
+          {make.toUpperCase()}&nbsp; {model.toUpperCase()}
         </h2>
       </div>
       <p className="flex mt-6 text-[32px] font-extrabold">
@@ -36,36 +35,35 @@ const CarCard = ({ car }: CarCardProps) => {
           className="object-contain"
         />
       </div>
+
       <div className="relative flex w-full mt-2">
-        <div className="flex group-hover:invisible w-full justify-between text-gray-100">
+        <div className="flex group-hover:invisible w-full justify-between text-grey">
           <div className="flex flex-col justify-center items-center gap-2">
             <Image
               src="/steering-wheel.svg"
-              alt="steering wheel"
               width={20}
               height={20}
+              alt="steering wheel"
             />
-            <p className="text-[14px]">
+            <p className="text-[14px] leading-[17px]">
               {transmission === "a" ? "Automatic" : "Manual"}
             </p>
           </div>
-
-          <div className="flex flex-col justify-center items-center gap-2">
-            <Image src="/tire.svg" alt="tire" width={20} height={20} />
-            <p className="text-[14px]">{drive.toUpperCase()}</p>
+          <div className="car-card__icon">
+            <Image src="/tire.svg" width={20} height={20} alt="seat" />
+            <p className="car-card__icon-text">{drive.toUpperCase()}</p>
           </div>
-
-          <div className="flex flex-col justify-center items-center gap-2">
-            <Image src="/gas.svg" alt="gas" width={20} height={20} />
-            <p className="text-[14px]">{city_mpg} MPG</p>
+          <div className="car-card__icon">
+            <Image src="/gas.svg" width={20} height={20} alt="seat" />
+            <p className="car-card__icon-text">{city_mpg} MPG</p>
           </div>
         </div>
-        <div className="car-card__btn-container">
+        <div className="hidden group-hover:flex absolute bottom-0 w-full z-10">
           <CustomButton
             btnType="button"
             title="View More"
             rightIcon="/right-arrow.svg"
-            containerStyles="w-full py-[16px]"
+            containerStyles="w-full py-[16px] bg-primary-blue"
             textStyles="text-white text-[14px] leading-[17px] font-bold"
             handleClick={() => setIsOpen(true)}
           />
